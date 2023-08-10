aa71f98ea04b7bfc38940ac7195a67afe682f540
kiran-c8 <kiranc8298@gmail.com>
2023-08-11 02:33:23 +0530

    deleted unwanted comment

--- models/questionsSchema.js
+++ models/questionsSchema.js
@@ -1,25 +1,3 @@
-// const mongoose = require('mongoose');
-// const {Schema} = require('mongoose')
-// const questionsSchema = Schema({
-//     id: {
-//         type: String,
-//         unique: true,
-//         required: true,
-//     },
-//     text: String,
-//     options: [
-//         {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: 'Option',
-//         },
-//     ],
-// });
-
-
-// const Questions = mongoose.model('Questions',questionsSchema);
-
-// module.exports = Questions;
-
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
